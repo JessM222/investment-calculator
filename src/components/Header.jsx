a587d6f5d591b63235f694d17d0e5bf04df513e4
JessM222 <ymia678@aucklanduni.ac.nz>
2025-04-14 09:19:22 +1200

    adjusted style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,9 @@ import moneyBucketImage from "../assets/investment-calculator-logo.png";
 
 export default function Header() {
   return (
-    <div id="header">
-      <img src={moneyBucketImage} alt="moneyBucketImage" />
+    <header id="header">
+      <img src={moneyBucketImage} alt="logo" />
       <h1>Investment Calculator</h1>
-    </div>
+    </header>
   );
 }
-    
--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,43 +1,56 @@
 export default function InputGroup({ inputs, onChangeInput }) {
-  console.log("===== value", inputs);
   return (
     <section id="user-input">
       <div className="input-group">
-        <label>Initial Investment</label>
-        <input
-          type="number"
-          name="initialInvestment"
-          value={inputs.initialInvestment}
-          required
-          onChange={(event)=>{onChangeInput("initialInvestment",event.target.value)}}
-        />
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            name="initialInvestment"
+            value={inputs.initialInvestment}
+            required
+            onChange={(event) => {
+              onChangeInput("initialInvestment", event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <label>Annual Investment </label>
+          <input
+            type="number"
+            name="annualInvestment"
+            value={inputs.annualInvestment}
+            required
+            onChange={(event) => {
+              onChangeInput("annualInvestment", event.target.value);
+            }}
+          />
+        </p>
 
-        <label>Annual Investment </label>
-        <input
-          type="number"
-          name="annualInvestment"
-          value={inputs.annualInvestment}
-          required
-          onChange={(event)=>{onChangeInput("annualInvestment",event.target.value)}}
-        />
-
-        <label>Expected Return </label>
-        <input
-          type="number"
-          name="expectedReturn"
-          value={inputs.expectedReturn}
-          required
-          onChange={(event)=>{onChangeInput("expectedReturn",event.target.value)}}
-        />
-
-        <label>Duration </label>
-        <input
-          type="number"
-          name="duration"
-          value={inputs.duration}
-          required
-          onChange={(event)=>{onChangeInput("duration",event.target.value)}}
-        />
+        <p>
+          <label>Expected Return </label>
+          <input
+            type="number"
+            name="expectedReturn"
+            value={inputs.expectedReturn}
+            required
+            onChange={(event) => {
+              onChangeInput("expectedReturn", event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <label>Duration </label>
+          <input
+            type="number"
+            name="duration"
+            value={inputs.duration}
+            required
+            onChange={(event) => {
+              onChangeInput("duration", event.target.value);
+            }}
+          />
+        </p>
       </div>
     </section>
   );
